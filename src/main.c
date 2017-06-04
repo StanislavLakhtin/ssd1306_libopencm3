078562033b2c584343c35d4f5718e87762ab536a
Stanislav Lakhtin <savant@savant.pro>
2017-06-04 22:41:54 +0300

    First experiments with i2c. It's not worked right now!

--- src/main.c
+++ src/main.c
@@ -30,28 +30,82 @@
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/exti.h>
 #include <libopencm3/stm32/f1/gpio.h>
+#include <libopencm3/stm32/i2c.h>
+
+#include <ssd1306_i2c.h>
 
 void clock_setup(void) {
   rcc_clock_setup_in_hse_12mhz_out_72mhz();
 
-  /* Enable GPIOA, GPIOB, GPIOC clock. */
+  /* Enable GPIOs clock. */
   rcc_periph_clock_enable(RCC_GPIOA);
   rcc_periph_clock_enable(RCC_GPIOB);
   rcc_periph_clock_enable(RCC_GPIOC);
 
-  /* Enable clocks for GPIO port A (for GPIO_USART2_TX) and USART2. */
-  rcc_periph_clock_enable(RCC_AFIO);
+  /* Enable clocks for USART2. */
   rcc_periph_clock_enable(RCC_USART2);
+}
 
-  /* Enable SPI1 Periph and gpio clocks */
+static void i2c_setup(void) {
+  /* set clock for I2C */
   rcc_periph_clock_enable(RCC_I2C2);
+  rcc_periph_clock_enable(RCC_AFIO);
 
-  /* Enable DMA1 clock */
-  rcc_periph_clock_enable(RCC_DMA1);
+  /* Set alternate functions for the SCL and SDA pins of I2C2. */
+  gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
+                GPIO_CNF_OUTPUT_ALTFN_OPENDRAIN,
+                GPIO_I2C2_SCL | GPIO_I2C2_SDA);
+
+  /* Disable the I2C before changing any configuration. */
+  i2c_peripheral_disable(I2C2);
+
+  /* APB1 is running at 36MHz. */
+  i2c_set_clock_frequency(I2C2, I2C_CR2_FREQ_36MHZ);
+
+  /* 400KHz - I2C Fast Mode */
+  i2c_set_fast_mode(I2C2);
+
+  /*
+	 * fclock for I2C is 36MHz APB2 -> cycle time 28ns, low time at 400kHz
+	 * incl trise -> Thigh = 1600ns; CCR = tlow/tcycle = 0x1C,9;
+	 * Datasheet suggests 0x1e.
+	 */
+  i2c_set_ccr(I2C2, 0x1e);
+
+  /*
+   * fclock for I2C is 36MHz -> cycle time 28ns, rise time for
+   * 400kHz => 300ns and 100kHz => 1000ns; 300ns/28ns = 10;
+   * Incremented by 1 -> 11.
+   */
+  i2c_set_trise(I2C2, 0x0b);
+
+  /*
+   * This is our slave address - needed only if we want to receive from
+   * other masters.
+   *
+   * i2c_set_own_7bit_slave_address(I2C2, 0x32);
+   */
+
+  /* If everything is configured -> enable the peripheral. */
+  i2c_peripheral_enable(I2C2);
+}
+
+void exti9_5_isr(void) {
+  exti_reset_request(EXTI8); // we should clear flag manually
 }
 
 void board_setup(void) {
+  rcc_periph_clock_enable(RCC_GPIOA);
 
+  // Debug setting for rotary encoder EC11 on (PA8, PA9) for make simple command
+  gpio_set_mode(GPIOA, GPIO_MODE_INPUT,
+                GPIO_CNF_INPUT_FLOAT,
+                GPIO8 | GPIO9);
+
+  nvic_enable_irq(NVIC_EXTI9_5_IRQ);
+  exti_enable_request(EXTI8);
+  exti_set_trigger(EXTI8, EXTI_TRIGGER_FALLING);
+  exti_select_source(EXTI8, GPIOA);
 }
 
 int main(void) {
@@ -64,7 +118,10 @@ int main(void) {
   }
 
   clock_setup();
+  i2c_setup();
   board_setup();
+
+  ssd1306_init(I2C2, I2C_ADDRESS_SA0_0, 128, 32);
 
   while (1);
 
